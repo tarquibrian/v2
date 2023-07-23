89f7a5c973fe3c05c3606b8f013741a01a335ad2
brian tarqui <tarquibrian@gmail.com>
2023-07-23 18:21:21 -0400

    test

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -4,43 +4,41 @@ import Link from "next/link";
 import React from "react";
 import logo from "../../../public/img/darvy-icon.png";
 import { motion } from "framer-motion";
+import { LineY } from "@/utils/line";
 
 const Navbar = () => {
   return (
     <header id="navbar">
       <div className="navbar-padding">
-        <div className="navbar-content">
-          <div className="nav">
-            <div className="logo">
-              <Image src={logo} alt="logo icon" width={50} height={50} />
-            </div>
-            <div className="navbar-content-main">
-              <nav>
-                <Link href={"/"}>
-                  <span>SERVICES</span>
-                </Link>
-                <Link href={"/"}>
-                  <span>HOME</span>
-                </Link>
-                <Link href={"/"}>
-                  <span>HOME</span>
-                </Link>
-              </nav>
-              <div className="options">
-                <Link href={"/"}>
-                  <span>CONTACT</span>
-                </Link>
-              </div>
-            </div>
-          </div>
+        <div className="logo">
+          <Image src={logo} alt="logo icon" width={50} height={50} />
         </div>
-        <motion.div
-          className="line"
-          initial={{ width: "0%" }}
-          animate={{ width: "100%" }}
-          transition={{ duration: 1, delay: 0.3 }}
-        ></motion.div>
+        <div className="navbar-content-main">
+          <nav className="links-menu">
+            <Link href={"/"}>
+              <span>SERVICES</span>
+            </Link>
+            <Link href={"/"}>
+              <span>HOME</span>
+            </Link>
+            <Link href={"/"}>
+              <span>HOME</span>
+            </Link>
+          </nav>
+          <div className="options">
+            <Link href={"/"}>
+              <span>CONTACT</span>
+            </Link>
+          </div>
+          <div className="liney"></div>
+        </div>
       </div>
+      <motion.div
+        className="line"
+        initial={{ width: "0%" }}
+        animate={{ width: "100%" }}
+        transition={{ duration: 1, delay: 0.3 }}
+      ></motion.div>
     </header>
   );
 };
