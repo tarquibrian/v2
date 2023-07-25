3756ce608f4ec7fe67e187f6ee50fad6535392ef
brian tarqui <tarquibrian@gmail.com>
2023-07-25 15:14:57 -0400

    test

--- src/app/[locale]/work/[id]/sections/singleContent.tsx
+++ src/app/[locale]/work/[id]/sections/singleContent.tsx
@@ -10,33 +10,40 @@ const SingleContent = () => {
       <div className="singlecontent__section">
         <div className="singlecontent__section-title">
           <div className="title__content">
-            <div className="title__content-feature">
-              <h2>CHALLENGE</h2>
-              <p>
-                Our client needed a brand refresh to better reflect their
-                evolving business values and appeal to a younger demographic.
-              </p>
-              <div className="logo">L</div>
-            </div>
-            <div className="title__content-feature">
-              <h2>CHALLENGE</h2>
-              <p>
-                Our client needed a brand refresh to better reflect their
-                evolving business values and appeal to a younger demographic.
-              </p>
-              <div className="logo">L</div>
-            </div>
-            <div className="title__content-feature">
-              <h2>CHALLENGE</h2>
-              <p>
-                Our client needed a brand refresh to better reflect their
-                evolving business values and appeal to a younger demographic.
-              </p>
-              <div className="logo">L</div>
-            </div>
+            <Wrapper id="item" type="div">
+              <div className="title__content-sticky">
+                <div className="sticky-wrapper">
+                  <h2>CHALLENGE</h2>
+                  <p>
+                    Our client needed a brand refresh to better reflect their
+                    evolving business values and appeal to a younger
+                    demographic.
+                  </p>
+                  <div className="logo">L</div>
+                </div>
+                <div className="sticky-wrapper">
+                  <h2>CHALLENGE</h2>
+                  <p>
+                    Our client needed a brand refresh to better reflect their
+                    evolving business values and appeal to a younger
+                    demographic.
+                  </p>
+                  <div className="logo">L</div>
+                </div>
+                <div className="sticky-wrapper">
+                  <h2>CHALLENGE</h2>
+                  <p>
+                    Our client needed a brand refresh to better reflect their
+                    evolving business values and appeal to a younger
+                    demographic.
+                  </p>
+                  <div className="logo">L</div>
+                </div>
+              </div>
+            </Wrapper>
           </div>
         </div>
-        <LineY />
+        <LineY id="intern" />
         <div className="singlecontent__section-pictures">
           <div className="pictures__content">
             <Image src={imgtest} alt="picture image" />
