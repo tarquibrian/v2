6d3b38b4072f72ffc33e0a6e73a5498fe19194bf
brian tarqui <tarquibrian@gmail.com>
2023-07-25 13:38:30 -0400

    test

--- src/app/[locale]/work/[id]/sections/singleContent.tsx
+++ src/app/[locale]/work/[id]/sections/singleContent.tsx
@@ -1,14 +1,56 @@
-import Wrapper from '@/utils/wrapper'
-import React from 'react'
+import { LineY } from "@/utils/line";
+import Wrapper from "@/utils/wrapper";
+import React from "react";
+import imgtest from "../../../../../../public/img/brand.jpg";
+import Image from "next/image";
 
 const SingleContent = () => {
   return (
-    <Wrapper id='singlecontent'>
+    <Wrapper id="singlecontent">
       <div className="singlecontent__section">
-        content
+        <div className="singlecontent__section-title">
+          <div className="title__content">
+            <div className="title__content-feature">
+              <h2>CHALLENGE</h2>
+              <p>
+                Our client needed a brand refresh to better reflect their
+                evolving business values and appeal to a younger demographic.
+              </p>
+              <div className="logo">L</div>
+            </div>
+            <div className="title__content-feature">
+              <h2>CHALLENGE</h2>
+              <p>
+                Our client needed a brand refresh to better reflect their
+                evolving business values and appeal to a younger demographic.
+              </p>
+              <div className="logo">L</div>
+            </div>
+            <div className="title__content-feature">
+              <h2>CHALLENGE</h2>
+              <p>
+                Our client needed a brand refresh to better reflect their
+                evolving business values and appeal to a younger demographic.
+              </p>
+              <div className="logo">L</div>
+            </div>
+          </div>
+        </div>
+        <LineY />
+        <div className="singlecontent__section-pictures">
+          <div className="pictures__content">
+            <Image src={imgtest} alt="picture image" />
+          </div>
+          <div className="pictures__content">
+            <Image src={imgtest} alt="picture image" />
+          </div>
+          <div className="pictures__content">
+            <Image src={imgtest} alt="picture image" />
+          </div>
+        </div>
       </div>
     </Wrapper>
-  )
-}
+  );
+};
 
-export default SingleContent
+export default SingleContent;
