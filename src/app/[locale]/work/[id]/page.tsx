f43490e8660289a200163d23c1202b7e98b93882
brian tarqui <tarquibrian@gmail.com>
2023-07-27 01:28:20 -0400

    test

--- src/app/[locale]/work/[id]/page.tsx
+++ src/app/[locale]/work/[id]/page.tsx
@@ -3,8 +3,7 @@ import HeroSingleWork from "./sections/heroSingleWork";
 import SingleContent from "./sections/singleContent";
 import Contact from "../../sections/contact";
 
-const SingleProject = ({ params }) => {
-  console.log(params);
+const SingleProject = () => {
   return (
     <main>
       <HeroSingleWork />
