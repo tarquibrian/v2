bec86ebc6ae2dcde28026013d1d80462ca42b49a
brian tarqui <tarquibrian@gmail.com>
2023-07-27 23:09:31 -0400

    test

--- src/app/[locale]/components/icons/logo.tsx
+++ src/app/[locale]/components/icons/logo.tsx
@@ -3,7 +3,7 @@ import logo from "../../../../../public/img/logodarvy.png";
 import Image from "next/image";
 
 const LogoIcon = () => {
-  return <Image src={logo} alt="darvy portfolio icon" placeholder="blur" />;
+  return <Image src={logo} alt="darvy portfolio icon" />;
 };
 
 export default LogoIcon;
