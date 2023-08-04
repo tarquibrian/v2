cdbfcab3a133719e3c8a9061a2cb59749435b7b6
brian tarqui <tarquibrian@gmail.com>
2023-08-04 00:45:21 -0400

    test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,19 +5,19 @@ import Navbar from "./components/navbar";
 import Footer from "./components/footer";
 
 export const metadata: Metadata = {
-  title: "DARVY | DIGITAL FREELANCER",
+  title: "DARVY - Digital Freelancer",
   description:
     "I am a design and development enthusiast and keep up with the latest trends in web design and user experience. This allows me to implement innovative solutions in my projects.",
   openGraph: {
     url: "https://www.zonustech.com/",
     type: "website",
-    title: "DARVY | DIGITAL FREELANCER",
+    title: "DARVY - Digital Freelancer",
     siteName: "zonustech.com",
     images: [{ url: "https://zonustech.com/og.png", alt: "Zonustech OG" }],
   },
   twitter: {
     card: "summary_large_image",
-    title: "DARVY | DIGITAL FREELANCER",
+    title: "DARVY - Digital Freelancer",
     description:
       "I am a design and development enthusiast and keep up with the latest trends in web design and user experience. This allows me to implement innovative solutions in my projects.",
     images: ["https://zonustech.com/og.png"],
